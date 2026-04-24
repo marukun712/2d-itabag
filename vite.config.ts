eb567cf3fce931a5ef2ad3940b9ddd200badeef0
marukun712 <marukun530@gmail.com>
2026-04-24 23:09:50 +0900

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,34 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-	plugins: [VitePWA()],
+	plugins: [
+		VitePWA({
+			registerType: "autoUpdate",
+
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,psd}"],
+				maximumFileSizeToCacheInBytes: 50 * 1024 * 1024,
+				runtimeCaching: [
+					{
+						urlPattern: /\/models\/.+\.psd$/,
+						handler: "CacheFirst",
+						options: {
+							cacheName: "psd-models",
+							expiration: {
+								maxEntries: 5,
+								maxAgeSeconds: 60 * 60 * 24 * 30,
+							},
+						},
+					},
+				],
+			},
+			manifest: {
+				name: "2d-itabag",
+				short_name: "itabag",
+				display: "fullscreen",
+			},
+		}),
+	],
 	server: {
 		allowedHosts: ["nixos-develop.tail4618d.ts.net"],
 	},
